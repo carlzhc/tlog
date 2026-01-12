861ab98d4963526386ec7fe78691d93ca283dca8
carlzhc <356889@gmail.com>
2026-01-12 06:14:45 +0000

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.carlzhc/tlog "0.1.1-SNAPSHOT"
+(defproject com.github.carlzhc/tlog "0.1.1"
   :description "A tiny log for Clojure"
   :url "https://github.com:carlzhc/tlog"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
